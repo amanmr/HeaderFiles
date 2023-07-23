0ff382b6a21a608f0a9e7be367985210c9395839
Aman Mishra <iamamanmr@gmail.com>
2023-07-23 17:52:46 +0530

    Added binary tree traversals

--- treeFile.h
+++ treeFile.h
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <queue>
-#include<vector>
+#include <vector>
 using namespace std;
 
 class treeNode
 {
-private:
+public:
   int data;
   treeNode *left;
   treeNode *right;
 
-  treeNode *buildTreePIHelper(vector<int> &preorder, vector<int> &inorder, int s, int e, int &i)
+  treeNode(int d) // constructor
+  {
+    data = d;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+
+//....................BUILD FROM PREORDER AND INORDER HELPER......................//
+
+treeNode *buildTreePIHelper(vector<int> &preorder, vector<int> &inorder, int s, int e, int &i)
   {
     if (s > e)
       return NULL;
@@ -30,25 +41,40 @@ private:
     return root;
   }
 
-public:
-  treeNode(int d) // constructor
-  {
-    data = d;
-    left = NULL;
-    right = NULL;
-  }
-
+//....................BUILD FROM PREORDER AND INORDER......................//
   treeNode *buildTreePI(vector<int> &preorder, vector<int> &inorder)
   {
     int i = 0;
     return buildTreePIHelper(preorder, inorder, 0, preorder.size() - 1, i);
   }
+
+  //....................PREORDER......................//
   void preorderPrint(treeNode *root)
   {
-    if (root == NULL)
-      return;
+    if (root == NULL) return;
     cout << root->data << " ";
     preorderPrint(root->left);
     preorderPrint(root->right);
   }
-};
+
+
+  //....................INORDER......................//
+  void inorderPrint(treeNode *root) 
+  {
+    if (root == NULL) return;
+    inorderPrint(root->left);
+    cout << root->data << " ";
+    inorderPrint(root->right);
+  }
+
+
+  //....................POSTORDER......................//
+  void postorderPrint(treeNode *root) 
+  {
+    if (root == NULL) return;
+    postorderPrint(root->left);
+    postorderPrint(root->right);
+    cout << root->data << " ";
+  }
+
+  
